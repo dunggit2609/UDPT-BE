67447e9a547098658b67e3d734fa681c21a4d32b
Thang Bach <thang5217113@gmail.com>
2022-05-04 23:16:24 +0700

    change mongodb user

--- src/services/auth/db.js
+++ src/services/auth/db.js
@@ -3,23 +3,20 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const debug = require('debug')('myw-api:db');
 
-const uri = "mongodb+srv://admin1:123@cluster0.msdkr.mongodb.net/Auth?retryWrites=true&w=majority";
+const uri = 'mongodb+srv://thangbach:123@cluster0.msdkr.mongodb.net/Auth?retryWrites=true&w=majority';
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
 async function connect() {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+	try {
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
 
-        console.log('connect auth database successfully!')
-    } catch (error) {
-        console.log(`connect auth database failed cause ${error}!`)
-
-    }
+		console.log('connect auth database successfully!');
+	} catch (error) {
+		console.log(`connect auth database failed cause ${error}!`);
+	}
 }
 
 module.exports = { connect };
--- src/services/payment/db.js
+++ src/services/payment/db.js
@@ -3,22 +3,19 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const debug = require('debug')('myw-api:db');
 
-const uri = "mongodb+srv://admin1:123@cluster0.msdkr.mongodb.net/Payment?retryWrites=true&w=majority";
-
-
+const uri = 'mongodb+srv://thangbach:123@cluster0.msdkr.mongodb.net/Payment?retryWrites=true&w=majority';
 
 async function connect() {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+	try {
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
 
-        console.log('connect Payment database successfully!')
-    } catch (error) {
-        console.log(`connect Payment database failed cause ${error}!`)
-
-    }
+		console.log('connect Payment database successfully!');
+	} catch (error) {
+		console.log(`connect Payment database failed cause ${error}!`);
+	}
 }
 
 module.exports = { connect };
