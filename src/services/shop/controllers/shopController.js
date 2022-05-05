e2c9da1a5fc4640e1d73405a1d26a9394a84f7d1
Thang <thang5217113@gmail.com>
2022-05-05 13:27:28 +0700

    add some constraint in creating a shop

--- src/services/shop/controllers/shopController.js
+++ src/services/shop/controllers/shopController.js
@@ -1,21 +1,34 @@
-
 const db = require('../db');
 const ShipperModel = db.Shipper;
 const jwt = require('jsonwebtoken');
-var apiResponse = require('../../../helpers/apiResponse')
-const cloneDeep = require('../../../utils/cloneDeep')
+var apiResponse = require('../../../helpers/apiResponse');
+const cloneDeep = require('../../../utils/cloneDeep');
 const { shopRepo } = require('../repopsitory/shopRepo');
 
+exports.create = [
+	async function(req, res) {
+		const { name, email, user_id } = req.body;
+		try {
+			const existedName = await shopRepo.findByName(name);
+			if (existedName) {
+				return apiResponse.conflictResponse(res, 'Shop name already exist');
+			}
 
+			const existedEmail = await shopRepo.findByEmail(email);
+			if (existedEmail) {
+				return apiResponse.conflictResponse(res, 'Shop Email already exist');
+			}
 
-exports.create = [async function (req, res) {
-    
-    try {
-        const shop = await shopRepo.create(req.body)
-        return apiResponse.successResponseWithData(res, "Success", shop) 
-    }
-    catch (err) {
-        console.log("err", err)
-        return apiResponse.ErrorResponse(res, "Cannot create user")
-    }
-}]
+			const existedUser_id = await shopRepo.findByUser_id(user_id);
+			if (existedUser_id) {
+				return apiResponse.conflictResponse(res, 'Shop owner already has a shop');
+			}
+
+			const shop = await shopRepo.create(req.body);
+			return apiResponse.successResponseWithData(res, 'Success', shop);
+		} catch (err) {
+			console.log('err', err);
+			return apiResponse.ErrorResponse(res, 'Cannot create shop');
+		}
+	}
+];
--- src/services/shop/repopsitory/shopRepo.js
+++ src/services/shop/repopsitory/shopRepo.js
@@ -1,63 +1,73 @@
-const shopSchema = require('../models/shop')
+const shopSchema = require('../models/shop');
 const { ObjectID } = require('bson');
 
 module.exports.shopRepo = {
-    async create(newShop) {
-        try {
-            const curDate = new Date()
-            let shop = new shopSchema({
-                name: newShop.name,
-                description: newShop.description,
-                business_cert: newShop.business_cert,
-                bank_account: newShop.bank_account,
-                email: newShop.email,
-                phone: newShop.phone,
-                user_id: newShop.user_id,
-                location: newShop.location,
-                review: [],
-                created_at: curDate,
-                updated_at: curDate,
-                _id: new ObjectID()
-            });
-            shop = shop.save();
-            return shop;
-        } catch (err) {
-            return
-        }
+	async create(newShop) {
+		try {
+			const curDate = new Date();
+			let shop = new shopSchema({
+				name: newShop.name,
+				description: newShop.description,
+				business_cert: newShop.business_cert,
+				bank_account: newShop.bank_account,
+				email: newShop.email,
+				phone: newShop.phone,
+				user_id: newShop.user_id,
+				location: newShop.location,
+				review: [],
+				//created_at: curDate,
+				//updated_at: curDate,
+				_id: new ObjectID()
+			});
+			shop = shop.save();
+			return shop;
+		} catch (err) {
+			return;
+		}
+	},
 
-    },
+	async findById(id) {
+		return await shopSchema.findOne({ _id: id });
+	},
 
-    async findById(id) {
-        return await shopSchema.findOne({ _id: id });
-    },
+	async update(id, payload) {
+		let shop = shopSchema.findOne({ _id: id });
 
-    async update(id, payload) {
-        let shop = shopSchema.findOne({ _id: id })
+		if (!shop) {
+			return;
+		}
+		const curDate = new Date();
 
-        if (!shop) {
-            return;
-        }
-        const curDate = new Date()
+		shop = {
+			...shop,
+			...{
+				name: newShop.name,
+				description: newShop.description,
+				business_cert: newShop.business_cert,
+				bank_account: newShop.bank_account,
+				email: newShop.email,
+				phone: newShop.phone,
+				user_id: newShop.user_id,
+				location: newShop.location,
+				review: [],
+				updated_at: curDate
+			}
+		};
 
-        shop = {
-            ...shop, ...{
-                name: newShop.name,
-                description: newShop.description,
-                business_cert: newShop.business_cert,
-                bank_account: newShop.bank_account,
-                email: newShop.email,
-                phone: newShop.phone,
-                user_id: newShop.user_id,
-                location: newShop.location,
-                review: [],
-                updated_at: curDate
-            }
-        }
+		shop = await shop.save();
 
-        shop = await shop.save();
+		return shop;
+	},
 
-        return shop;
+	async findByName(name) {
+		return await shopSchema.findOne({ name: name });
+	},
 
-    }
+	async findByEmail(email) {
+		return await shopSchema.findOne({ email: email });
+	},
 
-}
+	async findByUser_id(user_id) {
+		return await shopSchema.findOne({ user_id: user_id });
+	}
+};
